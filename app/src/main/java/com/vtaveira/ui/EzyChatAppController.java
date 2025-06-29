0cfd8055f7545cc9ebe7e2d7eeaadf9388da4ce0
Victor Rodrigues <rodrigueseti@outlook.com>
2025-06-29 14:31:15 -0300

    feat: implement initial JavaFX application structure with main class, controller, and FXML layout

---
+++ app/src/main/java/com/vtaveira/ui/EzyChatAppController.java
@@ -0,0 +1,14 @@
+package com.vtaveira.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EzyChatAppController {
+  @FXML
+  private Label welcomeText;
+
+  @FXML
+  protected void onHelloButtonClick() {
+    welcomeText.setText("Bem vindo ao EzyChat");
+  }
+}
