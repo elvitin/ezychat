e0254f7f9a1bc0c78cb4f5b87a053225b2130e95
Victor Rodrigues <rodrigueseti@outlook.com>
2025-07-24 13:55:07 -0300

    feat: [server] add RelationalUserRepository implementation with email lookup functionality

--- server/src/main/java/com/vtaveira/domain/repository/UserRepository.java
+++ server/src/main/java/com/vtaveira/domain/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public interface UserRepository {
   void save(User user);
 
-  Optional<User> findById(long id);
-
   Optional<User> findByUsername(String username);
+
+  Optional<User> findByEmail(String email);
 }
---
+++ server/src/main/java/com/vtaveira/infra/persistence/repository/RelationalUserRepository.java
@@ -0,0 +1,57 @@
+package com.vtaveira.infra.persistence.repository;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import com.vtaveira.domain.model.User;
+import com.vtaveira.domain.repository.UserRepository;
+import com.vtaveira.infra.persistence.entity.UserEntity;
+import com.vtaveira.infra.persistence.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Slf4j
+public class RelationalUserRepository implements UserRepository {
+
+  private final Dao<UserEntity, Long> userDao;
+
+  public RelationalUserRepository(ConnectionSource connectionSource) throws SQLException {
+    this.userDao = DaoManager.createDao(connectionSource, UserEntity.class);
+  }
+
+  public void save(User user) {
+    try {
+      this.userDao.createOrUpdate(UserMapper.toEntity(user));
+    } catch (SQLException e) {
+      log.error("error saving user: {}", user, e);
+    }
+  }
+
+
+  @Override
+  public Optional<User> findByUsername(String username) {
+    try {
+      var user = this.userDao.queryForEq("username", username);
+      return Optional.of(UserMapper.toDomain(user.getFirst()));
+    } catch (SQLException | NoSuchElementException e) {
+      log.error("error finding user by username {}", username, e);
+      return Optional.empty();
+    }
+  }
+
+
+  @Override
+  public Optional<User> findByEmail(String email) {
+    try {
+      var user = this.userDao.queryForEq("email", email);
+      return Optional.of(UserMapper.toDomain(user.getFirst()));
+    } catch (SQLException | NoSuchElementException e) {
+      log.error("error finding user by email {}", email, e);
+      return Optional.empty();
+    }
+  }
+}
+
