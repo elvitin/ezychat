e992024838aac9e351d24e1a3d3f51bd056a09c0
Victor Rodrigues <rodrigueseti@outlook.com>
2025-07-24 21:17:24 -0300

    refactor: update DatabaseConfig and ValidationUtil to use instance methods instead of static methods

--- app/src/main/java/com/vtaveira/infra/database/DatabaseConfig.java
+++ app/src/main/java/com/vtaveira/infra/database/DatabaseConfig.java
@@ -6,9 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @UtilityClass
 public class DatabaseConfig {
-  private static final String DATABASE_URL = "jdbc:sqlite:ezy_chat_client.db";
+  private final String DATABASE_URL = "jdbc:sqlite:ezy_chat_client.db";
 
-  public static void initialize() {
+  public void initialize() {
     log.info("initializing database with URL: {}", DATABASE_URL);
   }
 }
--- app/src/main/java/com/vtaveira/util/ValidationUtil.java
+++ app/src/main/java/com/vtaveira/util/ValidationUtil.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ValidationUtil {
 
-  public static boolean isValidEmail(String email) {
+  public boolean isValidEmail(String email) {
     if (email == null || email.trim().isEmpty()) {
       return false;
     }
