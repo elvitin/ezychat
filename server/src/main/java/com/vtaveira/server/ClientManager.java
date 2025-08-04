4c1661ee5f658173da8094d15fd76c9ed9c91f6c
Victor Rodrigues <rodrigueseti@outlook.com>
2025-08-04 15:58:59 -0300

    feat: [server] refactor ClientManager to use usernames instead of IPs for client management

--- server/src/main/java/com/vtaveira/server/ClientManager.java
+++ server/src/main/java/com/vtaveira/server/ClientManager.java
@@ -1,28 +1,35 @@
 package com.vtaveira.server;
 
 import com.vtaveira.builds.java.WrapperMessage;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 public class ClientManager {
-  private final ConcurrentHashMap<String, ClientHandler> onlineClients = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<String, ClientHandler> clients = new ConcurrentHashMap<>();
 
-  public void registerClient(String ip, ClientHandler handler) {
-    onlineClients.put(ip, handler);
+
+  public void registerClient(String username, ClientHandler handler) {
+    this.clients.put(username, handler);
   }
 
-  public void unregisterClient(String ip) {
-    if (ip != null) onlineClients.remove(ip);
+  public Optional<ClientHandler> getClient(String username) {
+    return this.clients.containsKey(username) ?
+        Optional.of(this.clients.get(username)) :
+        Optional.empty();
   }
 
-  public ClientHandler getClient(String ip) {
-    return onlineClients.get(ip);
+  public void unregisterClient(String username) {
+    Optional.ofNullable(username).ifPresent(this.clients::remove);
   }
 
-  public void broadcastMessage(WrapperMessage message, String excludeIp) {
-    for (ClientHandler clientHandler : onlineClients.values()) {
-      if (clientHandler.getIPAddress().equals(excludeIp)) continue;
-      clientHandler.sendMessage(message);
+  public void broadcastMessage(WrapperMessage message, String excludeUsername) {
+    for (var entry : this.clients.entrySet()) {
+      if (!entry.getKey().equals(excludeUsername)) {
+        entry.getValue().sendMessage(message);
+      }
     }
   }
 }
