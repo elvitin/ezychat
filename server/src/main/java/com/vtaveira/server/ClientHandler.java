c787a61e1798969de4dabeb674ff716b2472f484
Victor Rodrigues <rodrigueseti@outlook.com>
2025-08-04 16:05:57 -0300

    feat: [server] enhance ClientHandler to support AWS S3 storage and improve message handling

--- server/src/main/java/com/vtaveira/server/ClientHandler.java
+++ server/src/main/java/com/vtaveira/server/ClientHandler.java
@@ -1,16 +1,17 @@
 package com.vtaveira.server;
 
-
+import com.google.protobuf.ByteString;
 import com.vtaveira.builds.java.*;
 import com.vtaveira.domain.model.User;
 import com.vtaveira.domain.service.*;
+import com.vtaveira.infra.persistence.repository.AwsS3Storage;
 import com.vtaveira.main.factories.MessageServiceFactory;
 import com.vtaveira.main.factories.UserServiceFactory;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.sql.SQLException;
 
@@ -18,10 +19,10 @@ import java.sql.SQLException;
 public class ClientHandler implements Runnable {
   private final Socket socket;
   private final ClientManager clientManager;
+  private final AwsS3Storage fileStorage;
 
-
-  private final InputStream inFromClient; // to free
-  private final OutputStream outToClient; // to free
+  private final DataInputStream inFromClient; // to free
+  private final DataOutputStream outToClient; // to free
 
 
   //Services
@@ -33,9 +34,10 @@ public class ClientHandler implements Runnable {
 
 
   public ClientHandler(Socket socket, ClientManager clientManager) throws SQLException, IOException {
+    this.fileStorage = new AwsS3Storage();
     this.socket = socket;
-    this.inFromClient = socket.getInputStream();
-    this.outToClient = socket.getOutputStream();
+    this.inFromClient = new DataInputStream(socket.getInputStream());
+    this.outToClient = new DataOutputStream(socket.getOutputStream());
     this.clientManager = clientManager;
 
     this.userService = UserServiceFactory.getInstance();
@@ -43,13 +45,11 @@ public class ClientHandler implements Runnable {
 
     this.userSession.setIpAddress(Utils.getClientIPPort(socket));
 
-    Thread thread = new Thread(this);
+    var thread = new Thread(this);
     thread.setDaemon(true);
 
     thread.setName(this.userSession.getIpAddress());
     thread.start();
-
-    this.clientManager.registerClient(this.userSession.getIpAddress(), this);
 
     log.info("client connected: {}", this.userSession.getIpAddress());
   }
@@ -60,44 +60,63 @@ public class ClientHandler implements Runnable {
       while (this.socket.isConnected()) {
 
         log.debug("waiting for size of payload from client: [{}]", this.userSession.getIpAddress());
-        var len = this.inFromClient.read();
-        log.debug("payload size: [{}]", len);
+        var len = this.inFromClient.readInt();
         log.debug("reading {} bytes from client: [{}]", len, this.userSession.getIpAddress());
         var payload = WrapperMessage.parseFrom(inFromClient.readNBytes(len));
+
         log.debug("message received from client [{}], type [{}]", this.userSession.getIpAddress(), payload.getPayloadCase().name());
 
         switch (payload.getPayloadCase()) {
           case LOGINREQUEST -> this.handleLogin(payload.getLoginRequest());
           case REGISTERREQUEST -> this.handleRegister(payload.getRegisterRequest());
-          case PRIVATEMESSAGE -> this.handlePrivateMessage(payload.getPrivateMessage());
+          case PRIVATEMESSAGEREQUEST -> this.handlePrivateMessage(payload.getPrivateMessageRequest());
           case PAYLOAD_NOT_SET -> log.warn("payload not set");
           default -> log.warn("payload not recognized");
         }
       }
     } catch (IOException e) {
-      // Transformar em ENUM o 'Connection reset'.
+      this.clientManager.unregisterClient(this.userSession.getUsername());
+
       if (e.getLocalizedMessage().equals("Connection reset")) {
-        this.clientManager.unregisterClient(this.userSession.getIpAddress());
         log.warn("client disconnected: {}", this.userSession.getIpAddress());
         return;
       }
+
       log.error("unknown error, socket state {}: {}", this.socket.isConnected() ? "Connected" : "Disconnected", e.getLocalizedMessage());
     }
   }
 
-  private void handlePrivateMessage(PrivateMessage privateMessage) {
-    var receiverUsername = privateMessage.getRecipientUsername();
-    var content = privateMessage.getMessageContent();
-    this.messageService.sendPrivateMessage(this.userSession.getUsername(), receiverUsername, content, new SendPrivateMessageActions()
-        .onMessageSaved(() -> {
-          var ack = Ack.newBuilder()
-              .setSucess(true)
-              .setMessage("message sent successfully")
-              .build();
-          var payload = WrapperMessage.newBuilder().setAck(ack).build();
+  private void handlePrivateMessage(PrivateMessageRequest privateMessage) {
+    var toUsername = privateMessage.getToUsername();
+    var content = privateMessage.getContent();
+    this.messageService.save(this.userSession.getUsername(), toUsername, content, SendPrivateMessageActions.builder()
+        .onMessageSaved(savedMessage -> {
 
-          this.clientManager.getClient(receiverUsername).sendMessage(payload);
-        })
+          var response = PrivateMessageResponse.newBuilder()
+              .setSuccess(true)
+              .setId(savedMessage.id())
+              .build();
+
+
+          var payload = WrapperMessage.newBuilder()
+              .setPrivateMessageResponse(response)
+              .build();
+
+          this.sendMessage(payload);
+
+          var message = ChatMessage.newBuilder()
+              .setId(savedMessage.id())
+              .setUsername(this.userSession.getUsername())
+              .setFullName(savedMessage.fullName())
+              .setContent(savedMessage.content())
+              .setCreatedAt(savedMessage.createdAt().toInstant().toString());
+
+          var messagePayload = WrapperMessage.newBuilder()
+              .setChatMessage(message)
+              .build();
+
+          this.clientManager.getClient(toUsername).ifPresent(handler -> handler.sendMessage(messagePayload));
+        }).build()
     );
   }
 
@@ -107,13 +126,8 @@ public class ClientHandler implements Runnable {
 
     var response = LoginResponse.newBuilder();
 
-
     var actions = LoginActions.builder()
         .onSuccess(user -> {
-          response.setSuccess(true)
-              .setStatus(user.getStatus().toString())
-              .addAllPendingMessages(messageService.getPendingMessages(user.getUsername()));
-
           var statusPayload = UserStatusUpdate.newBuilder()
               .setUsername(user.getUsername())
               .setStatus(user.getStatus().toString())
@@ -122,11 +136,46 @@ public class ClientHandler implements Runnable {
           this.userSession.setUsername(user.getUsername());
           this.userSession.setStatus(user.getStatus());
 
+          this.clientManager.registerClient(user.getUsername(), this);
+
+          response.setSuccess(true)
+              .setStatus(user.getStatus().toString());
+
+
+          this.messageService.getPendingMessagesGroupedByConversation(user.getUsername()).entrySet()
+              .stream()
+              .map(entry -> {
+                    var conversation = Conversation.newBuilder()
+                        .setToUsername(entry.getKey().username())
+                        .setFriendFullName(entry.getKey().fullName())
+                        .addAllMessages(
+                            entry.getValue().stream()
+                                .map(message -> MessageRestored.newBuilder()
+                                    .setId(message.getId())
+                                    .setContent(message.getContent())
+                                    .setCreatedAt(message.getCreatedAt().toInstant().toString())
+                                    .setFromMe(message.getSender().getUsername().equals(user.getUsername()))
+                                    .build()
+                                ).toList()
+                        );
+                    this.fileStorage.load(entry.getKey().username()).ifPresent(file ->
+                        conversation.setFile(
+                            File.newBuilder()
+                                .setData(ByteString.copyFrom(file.content()))
+                                .setContentType(file.contentType())
+                                .build()
+                        )
+                    );
+                    return conversation;
+                  }
+              )
+              .forEach(response::addConversation);
+
           var notification = WrapperMessage.newBuilder()
               .setUserStatusUpdate(statusPayload)
               .build();
 
-          this.clientManager.broadcastMessage(notification, this.userSession.getIpAddress());
+          //this.clientManager.broadcastMessage(notification, this.userSession.getUsername());
         })
         .onUserNotFound(_ -> response.setSuccess(false).setErrorMessage("Usuário não encontrado"))
         .onInvalidPassword(_ -> response.setSuccess(false).setErrorMessage("Senha inválida"))
@@ -153,28 +202,34 @@ public class ClientHandler implements Runnable {
     var response = WrapperMessage.newBuilder();
     this.userService.register(user, new RegisterActions()
         .onUserRegistered(() -> {
-          var ack = Ack.newBuilder()
-              .setSucess(true)
+          var registerResponse = RegisterResponse.newBuilder()
+              .setSuccess(true)
               .setMessage("Usuário registrado com sucesso")
               .build();
-          response.setAck(ack);
+
+          if (registerRequest.hasProfilePicture()) {
+            var profilePicture = registerRequest.getProfilePicture();
+            var data = profilePicture.getData().toByteArray();
+            this.fileStorage.save(user.getUsername(), profilePicture.getContentType(), data);
+          }
+          response.setRegisterResponse(registerResponse);
         })
         .onUsernameAlreadyInUse(() -> {
           log.warn("username already in use: {}", registerRequest.getUsername());
-          var ack = Ack.newBuilder()
-              .setSucess(false)
+          var registerResponse = RegisterResponse.newBuilder()
+              .setSuccess(false)
               .setMessage("Nome de usuário já está em uso")
               .build();
-          response.setAck(ack);
+          response.setRegisterResponse(registerResponse);
         })
         .onEmailAlreadyInUse(() -> {
           log.warn("email already in use: {}", registerRequest.getEmail());
-          var ack = Ack.newBuilder()
-              .setSucess(false)
+          var registerResponse = RegisterResponse.newBuilder()
+              .setSuccess(false)
               .setMessage("Email já está em uso")
               .build();
 
-          response.setAck(ack);
+          response.setRegisterResponse(registerResponse);
         })
     );
     this.sendMessage(response.build());
@@ -182,17 +237,12 @@ public class ClientHandler implements Runnable {
 
   public void sendMessage(WrapperMessage payload) {
     try {
-      log.debug("sending message to client ip [{}]", this.userSession.getIpAddress());
-      var len = payload.getSerializedSize();
-      this.outToClient.write(len);
-      this.outToClient.write(payload.toByteArray());
-      log.debug("message sent to client ip [{}]", this.userSession.getIpAddress());
+      var bytes = payload.toByteArray();
+      this.outToClient.writeInt(bytes.length);
+      this.outToClient.write(bytes);
+      log.debug("message sent to client ip [{}] size [{}]", this.userSession.getIpAddress(), bytes.length);
     } catch (IOException e) {
       log.error("error sending message to client ip [{}]: {}", this.userSession.getIpAddress(), e.getLocalizedMessage());
     }
-  }
-
-  public String getIPAddress() {
-    return this.userSession.getIpAddress();
   }
 }
